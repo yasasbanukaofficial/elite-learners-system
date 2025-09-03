df064e12c09b947932e630afd12b9bbafe4e3e04
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-03 22:41:38 +0530

    feat(main): created FXML, controller - completed UI

---
+++ src/main/java/lk/ijse/learners/controller/MainLayoutController.java
@@ -0,0 +1,19 @@
+package lk.ijse.learners.controller;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+public class MainLayoutController {
+    public AnchorPane pageAnc;
+    public AnchorPane mainAnc;
+
+    public HBox btnStudentMgmt;
+    public HBox btnLessonMgmt;
+    public HBox btnPaymentMgmt;
+    public HBox btnCoursesMgmt;
+    public HBox btnStaffMgmt;
+    public HBox btnInstructorMgmt;
+    public HBox btnSettingsPage;
+    public ImageView btnLogout;
+}
