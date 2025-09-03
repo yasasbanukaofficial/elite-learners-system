9f26980bf0887177a7c9a9ead0274c3c3eb05fb0
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-03 22:45:21 +0530

    refactor(login): added id's to fields and buttons

--- src/main/java/lk/ijse/learners/controller/LoginPageController.java
+++ src/main/java/lk/ijse/learners/controller/LoginPageController.java
@@ -1,4 +1,17 @@
 package lk.ijse.learners.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginPageController {
+    public AnchorPane loginAnc;
+
+    public TextField usernameField;
+    public PasswordField passwordField;
+
+    public Label btnShowPassword;
+    public Button btnLogin;
 }
