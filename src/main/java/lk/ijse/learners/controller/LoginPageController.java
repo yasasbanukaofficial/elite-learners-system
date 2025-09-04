beeb3e8b4553564605cb0ecba038035451e8a232
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-04 16:21:29 +0530

    feat(main-layout): added user, student, instructor navigation paths

--- src/main/java/lk/ijse/learners/controller/LoginPageController.java
+++ src/main/java/lk/ijse/learners/controller/LoginPageController.java
@@ -1,10 +1,16 @@
 package lk.ijse.learners.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginPageController {
     public AnchorPane ancLogin;
@@ -14,4 +20,15 @@ public class LoginPageController {
 
     public Label btnShowPassword;
     public Button btnLogin;
+
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MainLayout.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.setMaximized(true);
+        stage.show();
+
+        Stage window = (Stage) ancLogin.getScene().getWindow();
+        window.close();
+    }
 }
--- src/main/java/lk/ijse/learners/controller/MainLayoutController.java
+++ src/main/java/lk/ijse/learners/controller/MainLayoutController.java
@@ -1,10 +1,16 @@
 package lk.ijse.learners.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
-public class MainLayoutController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainLayoutController implements Initializable {
     public AnchorPane ancMain;
     public AnchorPane ancPages;
 
@@ -12,8 +18,57 @@ public class MainLayoutController {
     public HBox btnLessonMgmt;
     public HBox btnPaymentMgmt;
     public HBox btnCoursesMgmt;
-    public HBox btnStaffMgmt;
+    public HBox btnUserMgmt;
     public HBox btnInstructorMgmt;
     public HBox btnSettingsPage;
     public ImageView btnLogout;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            ancPages.getChildren().clear();
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/StudentMgmtPage.fxml"));
+            anchorPane.prefWidthProperty().bind(ancPages.widthProperty());
+            anchorPane.prefHeightProperty().bind(ancPages.heightProperty());
+            ancPages.getChildren().add(anchorPane);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void visitStudentPgOnAction(MouseEvent mouseEvent) {
+        try {
+            ancPages.getChildren().clear();
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/StudentMgmtPage.fxml"));
+            anchorPane.prefWidthProperty().bind(ancPages.widthProperty());
+            anchorPane.prefHeightProperty().bind(ancPages.heightProperty());
+            ancPages.getChildren().add(anchorPane);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void visitUserMgmtPgOnAction(MouseEvent mouseEvent) {
+        try {
+            ancPages.getChildren().clear();
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/UserMgmtPage.fxml"));
+            anchorPane.prefWidthProperty().bind(ancPages.widthProperty());
+            anchorPane.prefHeightProperty().bind(ancPages.heightProperty());
+            ancPages.getChildren().add(anchorPane);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void visitInstructorsPgOnAction(MouseEvent mouseEvent) {
+        try {
+            ancPages.getChildren().clear();
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/InstructorMgmtPage.fxml"));
+            anchorPane.prefWidthProperty().bind(ancPages.widthProperty());
+            anchorPane.prefHeightProperty().bind(ancPages.heightProperty());
+            ancPages.getChildren().add(anchorPane);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
