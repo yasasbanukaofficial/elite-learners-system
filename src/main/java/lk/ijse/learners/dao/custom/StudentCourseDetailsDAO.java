50a4347ab38b076937413acb6a13bb46bc2f0869
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-05 22:43:11 +0530

    feat(dao): StudentCourseDetailsDAO and its impl

---
+++ src/main/java/lk/ijse/learners/dao/custom/StudentCourseDetailsDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.learners.dao.custom;
+
+import lk.ijse.learners.dao.CrudDAO;
+import lk.ijse.learners.entity.StudentCourseDetails;
+
+public interface StudentCourseDetailsDAO extends CrudDAO<StudentCourseDetails> {
+}
