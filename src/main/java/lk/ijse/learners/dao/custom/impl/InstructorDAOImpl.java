c458729ada2951f455d822b8aed56fc8b119b2fd
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-05 15:52:39 +0530

    feat(dao): InstructorDAO and its impl

---
+++ src/main/java/lk/ijse/learners/dao/custom/impl/InstructorDAOImpl.java
@@ -0,0 +1,98 @@
+package lk.ijse.learners.dao.custom.impl;
+
+import lk.ijse.learners.config.FactoryConfiguration;
+import lk.ijse.learners.dao.custom.InstructorDAO;
+import lk.ijse.learners.entity.Instructor;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public class InstructorDAOImpl implements InstructorDAO {
+    FactoryConfiguration factoryConfiguration = FactoryConfiguration.getInstance();
+    @Override
+    public List<Instructor> getAll() throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Query<Instructor> query = session.createQuery("from Instructor", Instructor.class);
+            return query.list() == null ? null : query.list();
+        }
+    }
+
+    @Override
+    public String getLastId() throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Query<String> query = session.createQuery("select i.id from Instructor i order by i.id desc", String.class).setMaxResults(1);
+            return query.list() == null ? null : query.list().get(0);
+        }
+    }
+
+    @Override
+    public boolean save(Instructor instructor) throws Exception {
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(instructor);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean update(Instructor instructor) throws Exception {
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(instructor);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Instructor instructor = session.get(Instructor.class, id);
+            session.remove(instructor);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<String> getAllIds() throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Query<String> query = session.createQuery("select i.id from Instructor i", String.class);
+            return query.list() == null ? null : query.list();
+        }
+    }
+
+    @Override
+    public Optional<Instructor> findById(String id) throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Instructor instructor = session.get(Instructor.class, id);
+            return Optional.ofNullable(instructor);
+        }
+    }
+}
