4ff405da4ea482f80348f5abb3928b7286877c19
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-05 22:09:42 +0530

    feat(dao): CourseDAO and its impl

---
+++ src/main/java/lk/ijse/learners/dao/custom/impl/CourseDAOImpl.java
@@ -0,0 +1,98 @@
+package lk.ijse.learners.dao.custom.impl;
+
+import lk.ijse.learners.config.FactoryConfiguration;
+import lk.ijse.learners.dao.custom.CourseDAO;
+import lk.ijse.learners.entity.Course;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CourseDAOImpl implements CourseDAO {
+    private final FactoryConfiguration factoryConfiguration = FactoryConfiguration.getInstance();
+    @Override
+    public List<Course> getAll() throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Query<Course> query = session.createQuery("from Course", Course.class);
+            return query.list() == null ? null : query.list();
+        }
+    }
+
+    @Override
+    public String getLastId() throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Query<String> query = session.createQuery("select i.id from Course i order by i.id desc", String.class).setMaxResults(1);
+            return query.list() == null ? null : query.list().getFirst();
+        }
+    }
+
+    @Override
+    public boolean save(Course course) throws Exception {
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(course);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean update(Course course) throws Exception {
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(course);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Course course = session.get(Course.class, id);
+            session.remove(course);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<String> getAllIds() throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Query<String> query = session.createQuery("select i.id from Course i", String.class);
+            return query.list() == null ? null : query.list();
+        }
+    }
+
+    @Override
+    public Optional<Course> findById(String id) throws Exception {
+        try (Session session = factoryConfiguration.getSession()) {
+            Course course = session.get(Course.class, id);
+            return Optional.ofNullable(course);
+        }
+    }
+}
