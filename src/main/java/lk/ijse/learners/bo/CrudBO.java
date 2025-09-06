3628b49617d96d7268eed90e7bd8cd8dfe155f3b
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-07 00:16:47 +0530

    refactor(crud-bo): replace parameter name from entity to dto

--- src/main/java/lk/ijse/learners/bo/CrudBO.java
+++ src/main/java/lk/ijse/learners/bo/CrudBO.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 public interface CrudBO<T> extends SuperBO {
     List<T> getAll() throws Exception;
     String getLastId() throws Exception;
-    boolean save(T entity) throws Exception;
-    boolean update(T entity) throws Exception;
+    boolean save(T dto) throws Exception;
+    boolean update(T dto) throws Exception;
     boolean delete(String id) throws Exception;
     List<String> getAllIds() throws Exception;
     Optional<T> findById(String id) throws Exception;
