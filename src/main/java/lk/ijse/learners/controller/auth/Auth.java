d1ecce4a9098eade838e513cff4f3e03a6712031
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-08 00:18:33 +0530

    feat(auth, util): introduce auth and alert util for reusability

--- src/main/java/lk/ijse/learners/controller/auth/Auth.java
+++ src/main/java/lk/ijse/learners/controller/auth/Auth.java
@@ -1,4 +1,12 @@
 package lk.ijse.learners.controller.auth;
 
 public class Auth {
+    public static boolean areRequiredFieldsFilled(Object... inputs) {
+        for(Object input : inputs){
+            if (input == null || input.equals("")) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
