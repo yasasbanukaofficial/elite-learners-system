99f6172bbadad42799d432130fbe4a48c2164d29
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-07 17:49:11 +0530

    feat(ui): designed student management page ui

--- src/main/java/lk/ijse/learners/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/learners/config/FactoryConfiguration.java
@@ -6,7 +6,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.io.FileInputStream;
 import java.util.Properties;
 
 public class FactoryConfiguration {
--- src/main/java/lk/ijse/learners/controller/StudentMgmtPageController.java
+++ src/main/java/lk/ijse/learners/controller/StudentMgmtPageController.java
@@ -1,7 +1,15 @@
 package lk.ijse.learners.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 public class StudentMgmtPageController {
     public AnchorPane ancStudent;
+    public Button btnAddStudent;
+    public TableView tblStudents;
+
+    public void addStudent(ActionEvent actionEvent) {
+    }
 }
