f71d361ccd4fb0c3f23c0ae3cbb1656b4ee6a0d2
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-08 15:02:05 +0530

    feat(ui): added payment form and its controller

---
+++ src/main/java/lk/ijse/learners/controller/AddPaymentFormController.java
@@ -0,0 +1,24 @@
+package lk.ijse.learners.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class AddPaymentFormController {
+    public AnchorPane ancAddPayment;
+    public Label lblPayId;
+    public ImageView btnCloseStdForm;
+    public DatePicker dpPayDate;
+    public TextField txtAmount;
+    public CheckBox cbCard;
+    public CheckBox cbCash;
+    public Button btnCancel;
+    public Button btnPay;
+
+    public void closeStudentForm(ActionEvent actionEvent) {
+    }
+
+    public void addPayment(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/lk/ijse/learners/controller/ChooseCourseFormController.java
+++ src/main/java/lk/ijse/learners/controller/ChooseCourseFormController.java
@@ -2,26 +2,17 @@ package lk.ijse.learners.controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import lk.ijse.learners.bo.BOFactory;
 import lk.ijse.learners.bo.custom.StudentBO;
-import lk.ijse.learners.controller.auth.Auth;
 import lk.ijse.learners.controller.util.AlertUtil;
 import lk.ijse.learners.dto.StudentDTO;
 
-import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class ChooseCourseFormController implements Initializable {
@@ -56,16 +47,5 @@ public class ChooseCourseFormController implements Initializable {
             AlertUtil.setErrorAlert("Failed when saving student");
             e.printStackTrace();
         }
-    }
-
-    public void viewCourses(ActionEvent actionEvent) throws IOException {
-        System.out.println("Hi");
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/ChooseCourseForm.fxml"));
-        Scene scene = new Scene(parent);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.setMaximized(false);
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.show();
     }
 }
