92a4e798815ad11288e80e8bbe6c44bb909b0c71
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-10 13:44:20 +0530

    feat(bo-context): add enrollment unit of work for transaction state management

--- src/main/java/lk/ijse/learners/bo/context/EnrollmentUnitOfWork.java
+++ src/main/java/lk/ijse/learners/bo/context/EnrollmentUnitOfWork.java
@@ -1,4 +1,32 @@
 package lk.ijse.learners.bo.context;
 
+import lk.ijse.learners.dto.CourseDTO;
+import lk.ijse.learners.dto.PaymentDTO;
+import lk.ijse.learners.dto.StudentDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class EnrollmentUnitOfWork {
+    private static EnrollmentUnitOfWork enrollmentUnitOfWork = new EnrollmentUnitOfWork();
+
+    private StudentDTO studentDTO;
+    private PaymentDTO paymentDTO;
+    private CourseDTO courseDTO;
+
+    public static EnrollmentUnitOfWork getInstance() {
+        return enrollmentUnitOfWork == null ? enrollmentUnitOfWork = new EnrollmentUnitOfWork() : enrollmentUnitOfWork;
+    }
+
+    public void clear() {
+        studentDTO = null;
+        paymentDTO = null;
+        courseDTO = null;
+    }
+
 }
