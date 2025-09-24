d9c689dd7fee7452b53102e148c862dc83bfd886
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-24 17:23:12 +0530

    feat(user-mgmt-ui): added user mgmt ui

--- src/main/java/lk/ijse/learners/controller/UserMgmtPageController.java
+++ src/main/java/lk/ijse/learners/controller/UserMgmtPageController.java
@@ -1,7 +1,37 @@
 package lk.ijse.learners.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
-public class UserMgmtPageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserMgmtPageController implements Initializable {
+
     public AnchorPane ancUser;
+    public StackPane btnOpenStdForm;
+    public TextField txtUserName;
+    public TextField txtRole;
+    public TextField txtEmail;
+    public TextField txtAge;
+    public TextField txtContact;
+    public DatePicker userDob;
+    public Label btnShowPassword;
+    public PasswordField userPassword;
+    public Button btnDeleteUser;
+    public Button btnEdit;
+    public ListView listUsers;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void deleteUser(ActionEvent event) {
+    }
+
+    public void editUser(ActionEvent event) {
+    }
 }
