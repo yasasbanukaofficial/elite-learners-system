44d4fbd7bcc0c051ca7a12c41829650beec45c34
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-09-24 22:25:34 +0530

    feat(login): added function to toggle visibility of the password

--- src/main/java/lk/ijse/learners/controller/LoginPageController.java
+++ src/main/java/lk/ijse/learners/controller/LoginPageController.java
@@ -1,35 +1,51 @@
 package lk.ijse.learners.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.learners.controller.util.AlertUtil;
 import lk.ijse.learners.controller.util.ViewPath;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginPageController {
+public class LoginPageController implements Initializable {
     public AnchorPane ancLogin;
 
     public TextField usernameField;
     public PasswordField passwordField;
-
-    public Label btnShowPassword;
     public Button btnLogin;
+    public TextField textField;
+    public CheckBox showPassword;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        textField.managedProperty().bind(showPassword.selectedProperty());
+        textField.visibleProperty().bind(showPassword.selectedProperty());
+        passwordField.managedProperty().bind(showPassword.selectedProperty().not());
+        passwordField.visibleProperty().bind(showPassword.selectedProperty().not());
+        textField.textProperty().bindBidirectional(passwordField.textProperty());
+    }
+
+    @FXML
     public void loginOnAction(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(ViewPath.MAIN.getPath()));
-        Stage stage = new Stage();
-        stage.setScene(new Scene(fxmlLoader.load()));
-        stage.setMaximized(true);
-        stage.show();
+        if (usernameField.getText().equals("root") && passwordField.getText().equals("root")) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(ViewPath.MAIN.getPath()));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(fxmlLoader.load()));
+            stage.setMaximized(true);
+            stage.show();
 
-        Stage window = (Stage) ancLogin.getScene().getWindow();
-        window.close();
+            Stage window = (Stage) ancLogin.getScene().getWindow();
+            window.close();
+        } else {
+            AlertUtil.setErrorAlert("Invalid username or password");
+        }
     }
 }
